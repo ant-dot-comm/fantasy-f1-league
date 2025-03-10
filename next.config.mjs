48396c27f09eb1cbd92014cdfebb946f58538cb4
AntDotComm <32787612+ant-dot-comm@users.noreply.github.com>
2025-03-09 18:59:45 -0700

    Setup GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
     NEXT_PUBLIC_MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
     MONGODB_URI: process.env.MONGODB_URI,
   },
+  output: "export",
+  basePath: "/fantasy-f1-league",
 };
 
 export default nextConfig;
